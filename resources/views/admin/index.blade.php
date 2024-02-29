a1da3b90d098b741a6d4c44e87b9f826915fdd49
Brian Valles <briansitovalles@gmail.com>
2024-02-29 14:25:49 -0600

    Se empezo a diseñar las vistas, se integro adminlte para la administracion, y jetstream se usara para lo que vera el usuario comun.

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,19 @@
+@extends('adminlte::page')
+
+@section('title', 'Professional English for Bussines')
+
+@section('content_header')
+    <h1>Professional English for Bussines</h1>
+@stop
+
+@section('content')
+    <p>Welcome to this beautiful admin panel.</p>
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script> console.log('Hi!'); </script>
+@stop

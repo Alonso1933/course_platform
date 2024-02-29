43453e0a8be6f57c19805818e39759dfd77f1074
Brian Valles <briansitovalles@gmail.com>
2024-02-29 10:32:47 -0600

    Se agregaron los seeders 'Category, Level, Price, User' para rellenar con informacion de prueba la base de datos.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,5 +18,10 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        $this->call(UserSeeder::class);
+        $this->call(LevelSeeder::class);
+        $this->call(CategorySeeder::class);
+        $this->call(PriceSeeder::class);
     }
 }

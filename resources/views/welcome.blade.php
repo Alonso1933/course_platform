82c5bfb41d080778f1f3a2574bb57144ef0dbbfa
Brian Valles <briansitovalles@gmail.com>
2024-03-01 10:45:54 -0600

    Se modifico la plantilla Dashboard de jetstream, sera la vista que veran los usuarios. Se modifico la navbar de forma que tenga los mismos elementos en sus versiones PC y responsive, se usaron 'variables' y 'for each' Se cambio el comportamiento del logotipo, al hacer clic sobre el, nos mandara a la ruta principal ('home')

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,9 @@
-<!DOCTYPE html>
+<x-app-layout>
+
+</x-app-layout>
+
+
+{{-- <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
@@ -131,3 +136,4 @@
         </div>
     </body>
 </html>
+ --}}

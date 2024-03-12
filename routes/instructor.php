db046e9f49207ebc09e0baea637483877dfe5140
Brian Valles <briansitovalles@gmail.com>
2024-03-12 09:12:49 -0600

    Se inicio la creacion de la vista para los instructores, para esto no se administrar con un controlador sino con un componente de Livewire. Ademas de agregar un name y un prefix para las rutas de instructor,y de dicha forma mantener mas limpias las rutas. Se agrego una ruta vacia que automaticamente redigira a la ruta courses de instructor.

---
+++ routes/instructor.php
@@ -0,0 +1,10 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Livewire\InstructorCourses;
+
+Route::redirect('', 'instructor/courses');
+
+Route::get('courses', InstructorCourses::class)->name("courses.index");
+
+?>

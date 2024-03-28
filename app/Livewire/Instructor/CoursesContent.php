27526d163a8e0fd8625b77375a6ae89925cd55b0
Brian Valles <briansitovalles@gmail.com>
2024-03-28 12:54:22 -0600

    Se agrego una funcion al boton editar seccion, abre un formulario que muestra el nombre actuyal del curso, aun no tiene funcionalidad.

--- app/Livewire/Instructor/CoursesContent.php
+++ app/Livewire/Instructor/CoursesContent.php
@@ -3,18 +3,28 @@
 namespace App\Livewire\Instructor;
 
 use App\Models\Course;
+use App\Models\Section;
 use Livewire\Component;
 
 class CoursesContent extends Component
 {
-    public $course;
+    public $course, $section;
+
+    protected $rules = [
+        'section.name' => 'required'
+    ];
 
     public function mount(Course $course) {
         $this->course = $course;
+        $this->section = new Section();
     }
 
     public function render()
     {
         return view('livewire.instructor.courses-content')->layout('layouts.instructor');
+    }
+
+    public function edit(Section $section) {
+        $this->section = $section;
     }
 }
--- resources/views/livewire/instructor/courses-content.blade.php
+++ resources/views/livewire/instructor/courses-content.blade.php
@@ -7,18 +7,26 @@
     <div class="card-body text-gray-600">
         <h2 class="text-2xl font-bold">Lecciones del curso</h1>
         <hr class="mt-2 mb-6">
+
+        {{$section}}
         
         @foreach ($course->sections as $item)
             <article class="card mb-6">
                 <div class="card-body bg-gray-100">
-                    <header class="flex justify-between items-center">
-                        <h3 class="cursor-pointer"><strong>Sección: </strong>{{$item->name}}</h3>
+                    @if ($section->id == $item->id)
+                        <form>
+                            <input wire:model.live="section.name" class="form-input-css form" placeholder="Ingrese el nombre de la sección">
+                        </form>
+                    @else
+                        <header class="flex justify-between items-center">
+                            <h3 class="cursor-pointer"><strong>Sección: </strong>{{$item->name}}</h3>
 
-                        <div>
-                            <i class="fas fa-edit cursor-pointer text-blue-500"></i>
-                            <i class="fas fa-eraser cursor-pointer text-red-500"></i>
-                        </div>
-                    </header>
+                            <div>
+                                <i class="fas fa-edit cursor-pointer text-blue-500" wire:click="edit({{$item}})"></i>
+                                <i class="fas fa-eraser cursor-pointer text-red-500"></i>
+                            </div>
+                        </header>
+                    @endif
                 </div>
             </article>
         @endforeach

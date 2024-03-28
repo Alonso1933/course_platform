8be033790e7c4f2661694c2352bdd5702222e38d
Brian Valles <briansitovalles@gmail.com>
2024-03-28 12:06:54 -0600

    Se imprimieron las secciones del curso, y se colocaron los botones para editar y eliminar (sin funcionalidad

--- resources/views/livewire/instructor/courses-content.blade.php
+++ resources/views/livewire/instructor/courses-content.blade.php
@@ -3,4 +3,24 @@
     <x-slot name="course">
         {{$course->slug}}
     </x-slot>
+
+    <div class="card-body text-gray-600">
+        <h2 class="text-2xl font-bold">Lecciones del curso</h1>
+        <hr class="mt-2 mb-6">
+        
+        @foreach ($course->sections as $item)
+            <article class="card mb-6">
+                <div class="card-body bg-gray-100">
+                    <header class="flex justify-between items-center">
+                        <h3 class="cursor-pointer"><strong>Sección: </strong>{{$item->name}}</h3>
+
+                        <div>
+                            <i class="fas fa-edit cursor-pointer text-blue-500"></i>
+                            <i class="fas fa-eraser cursor-pointer text-red-500"></i>
+                        </div>
+                    </header>
+                </div>
+            </article>
+        @endforeach
+    </div>
 </div>

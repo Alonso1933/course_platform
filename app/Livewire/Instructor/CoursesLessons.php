ab04eb425a5849fa5833d8ad7e830ad19980471a
Brian Valles <briansitovalles@gmail.com>
2024-03-28 22:09:06 -0600

    Se creo una clase y vista con livewire para administrar las lecciones pertenecientes a un curso.

---
+++ app/Livewire/Instructor/CoursesLessons.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Instructor;
+
+use Livewire\Component;
+
+class CoursesLessons extends Component
+{
+    public function render()
+    {
+        return view('livewire.instructor.courses-lessons');
+    }
+}

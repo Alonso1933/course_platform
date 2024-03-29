62052324f85db7a547b093812b626416f4020bea
Brian Valles <briansitovalles@gmail.com>
2024-03-29 11:43:15 -0600

    Se agrego un campo para la descripcion de una leccion, para administrar esta accion se uso un componente livewire y de Alpine.

--- app/Livewire/Instructor/LessonDescription.php
+++ app/Livewire/Instructor/LessonDescription.php
@@ -7,7 +7,7 @@ use Livewire\Component;
 
 class LessonDescription extends Component
 {
-    public $lesson, $description;
+    public $lesson, $description, $name;
 
     protected $rules = [
         'description.name' => 'required'
@@ -24,5 +24,23 @@ class LessonDescription extends Component
     public function render()
     {
         return view('livewire.instructor.lesson-description');
+    }
+
+    public function store() {
+        $this->description = $this->lesson->description()->create(['name' => $this->name]);
+        $this->reset('name');
+
+        $this->lesson = Lesson::find($this->lesson->id);
+    }
+
+    public function update() {
+        $this->validate();
+        $this->description->save(); 
+    }
+
+    public function destroy() {
+        $this->description->delete(); 
+        $this->reset('description');
+        $this->lesson = Lesson::find($this->lesson->id);
     }
 }
